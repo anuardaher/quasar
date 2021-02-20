3dc26e0a9dd4a0d320937470abe26cdf92597219
Anuar <anuar_daher@hotmail.com>
2021-02-20 04:53:01 -0300

    retirando imagens desnecessarias

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
+    <q-header class="bg-drawer">
       <q-toolbar>
         <q-btn
           flat
@@ -13,15 +13,21 @@
         <q-toolbar-title>
           Budget
         </q-toolbar-title>
-        <q-space/>
+        <q-space />
         <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
-                 @click="$q.fullscreen.toggle()"
-                 v-if="$q.screen.gt.sm">
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+            @click="$q.fullscreen.toggle()"
+            v-if="$q.screen.gt.sm"
+          >
           </q-btn>
           <q-btn round flat>
             <q-avatar size="26px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-btn>
         </div>
@@ -31,13 +37,13 @@
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
-      content-class="bg-primary text-white"
+      dark
+      content-class="bg-drawer"
     >
       <q-list>
         <q-item to="/" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
-            <q-icon name="home"/>
+            <q-icon name="home" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Home</q-item-label>
@@ -45,20 +51,17 @@
         </q-item>
         <q-item to="/orcamento" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
-            <q-icon name="money"/>
+            <q-icon name="money" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Orçamento</q-item-label>
           </q-item-section>
         </q-item>
-        <q-expansion-item
-          icon="pages"
-          label="Pages"
-        >
+        <q-expansion-item icon="pages" label="Pages">
           <q-list class="q-pl-lg">
             <q-item to="/Login-1" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="email"/>
+                <q-icon name="email" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Login-1</q-item-label>
@@ -66,7 +69,7 @@
             </q-item>
             <q-item to="/Lock" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="lock"/>
+                <q-icon name="lock" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Lock Screen</q-item-label>
@@ -74,7 +77,7 @@
             </q-item>
             <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="lock"/>
+                <q-icon name="lock" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Lock Screen - 2</q-item-label>
@@ -82,24 +85,29 @@
             </q-item>
             <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="list"/>
+                <q-icon name="list" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Pricing</q-item-label>
               </q-item-section>
             </q-item>
-            <q-item-label header class="text-weight-bolder text-white">Generic</q-item-label>
+            <q-item-label header class="text-weight-bolder text-white"
+              >Generic</q-item-label
+            >
             <q-item to="/Profile" active-class="q-item-no-link-highlighting">
               <q-item-section avatar>
-                <q-icon name="person"/>
+                <q-icon name="person" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>User Profile</q-item-label>
               </q-item-section>
             </q-item>
-            <q-item to="/Maintenance" active-class="q-item-no-link-highlighting">
+            <q-item
+              to="/Maintenance"
+              active-class="q-item-no-link-highlighting"
+            >
               <q-item-section avatar>
-                <q-icon name="settings"/>
+                <q-icon name="settings" />
               </q-item-section>
               <q-item-section>
                 <q-item-label>Maintenance</q-item-label>
@@ -110,28 +118,38 @@
       </q-list>
     </q-drawer>
 
-    <q-page-container class="bg-grey-2">
-      <router-view/>
+    <q-page-container class="bg-page">
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-    import EssentialLink from 'components/EssentialLink'
-    import Messages from "./Messages";
+import EssentialLink from "components/EssentialLink";
+import Messages from "./Messages";
 
-    export default {
-        name: 'MainLayout',
+export default {
+  name: "MainLayout",
 
-        components: {
-            Messages,
-            EssentialLink
-        },
+  components: {
+    Messages,
+    EssentialLink
+  },
 
-        data() {
-            return {
-                leftDrawerOpen: false,
-            }
-        }
-    }
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  }
+};
 </script>
+
+<style>
+.bg-page {
+background-image: linear-gradient(to top, #d5d4d0 0%, #d5d4d0 1%, #eeeeec 31%, #efeeec 75%, #e9e9e7 100%);
+}
+
+.bg-drawer {
+background-image: linear-gradient(-20deg, #1e4057 0%, #332b50 100%);
+}
+</style>
